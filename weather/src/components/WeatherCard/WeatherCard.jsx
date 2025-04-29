ae80557e6d41778a34d0f6fc90f2cb98f0cfc5a3
YasmeenOthman <yasmeen.othman20@gmail.com>
2025-04-29 23:05:00 +0300

    weathercard static data'

--- weather/src/components/WeatherCard/WeatherCard.jsx
+++ weather/src/components/WeatherCard/WeatherCard.jsx
@@ -1,12 +1,27 @@
 // src/components/WeatherCard/WeatherCard.jsx
-import './WeatherCard.css';
+import "./WeatherCard.css";
 
 function WeatherCard() {
   return (
     <div className="weather-card">
-      <h2>28°C</h2>
-      <p>Rainy Storm Clouds</p>
-      <p>Florida, US</p>
+      <div className="search-bar">
+        <input type="text" placeholder="Search city..." />
+        <button>🔍</button>
+      </div>
+      <div className="weather-top">
+        <img
+          src="https://openweathermap.org/img/wn/11d@2x.png"
+          alt="Weather Icon"
+          className="weather-icon"
+        />
+        <div className="temperature">28°C</div>
+      </div>
+
+      <div className="weather-info">
+        <p className="weather-description">Rainy Storm Clouds</p>
+        <p className="location">Florida, US</p>
+        <p className="datetime">24 July, 2022 5:01 AM</p>
+      </div>
     </div>
   );
 }
