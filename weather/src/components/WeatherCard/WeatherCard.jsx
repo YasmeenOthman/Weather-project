fcec7431b467bdf03ce16bef16bdc064d5626b40
YasmeenOthman <yasmeen.othman20@gmail.com>
2025-05-05 12:25:49 +0300

    services and weather card data

--- weather/src/components/WeatherCard/WeatherCard.jsx
+++ weather/src/components/WeatherCard/WeatherCard.jsx
@@ -1,13 +1,48 @@
 // src/components/WeatherCard/WeatherCard.jsx
+import { useState, useEffect } from "react";
+import { fetchCurrentWeather } from "../../services/weatherService";
 import "./WeatherCard.css";
 
 function WeatherCard() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await fetchCurrentWeather(city);
+      setWeatherData(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+  useEffect(() => {
+    const getWeather = async () => {
+      try {
+        const data = await fetchCurrentWeather("Palestine");
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Failed to fetch weather:", error);
+      }
+    };
+
+    getWeather();
+  }, []);
+
   return (
     <div className="weather-card">
-      <div className="search-bar">
-        <input type="text" placeholder="Search city..." />
-        <button>🔍</button>
-      </div>
+      <form className="search-bar" onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search city..."
+          onChange={handleChange}
+        />
+        <button type="submit">🔍</button>
+      </form>
       <div className="weather-top">
         <div className="weather-icon-container">
           <img
@@ -16,15 +51,24 @@ function WeatherCard() {
             className="weather-icon"
           />
         </div>
-        <div className="temperature">28°C</div>
+        <div className="temperature">
+          {weatherData && weatherData.main.temp}°C
+        </div>
         <div className="weather-description-container">
-          <p className="weather-description">Rainy Storm Clouds</p>
+          <p className="weather-description">
+            {weatherData && weatherData.weather[0].description}
+          </p>
         </div>
       </div>
 
       <div className="weather-info">
-        <p className="location">Florida, US</p>
-        <p className="datetime">24 July, 2022 5:01 AM</p>
+        <p className="location">
+          {weatherData && weatherData.name},{" "}
+          {weatherData && weatherData.sys.country}
+        </p>
+        <p className="datetime">
+          {new Date(weatherData && weatherData.dt * 1000).toLocaleDateString()}
+        </p>
       </div>
     </div>
   );
--- weather/src/services/weatherService.js
+++ weather/src/services/weatherService.js
@@ -1,1 +1,27 @@
 // API call functions
+import axios from "axios";
+
+const API_KEY = "ef61d070814d087a0085c9fbe8bf6724"; // replace with your key
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+export const fetchCurrentWeather = async (city) => {
+  const response = await axios.get(`${BASE_URL}/weather`, {
+    params: {
+      q: city,
+      units: "metric",
+      appid: API_KEY,
+    },
+  });
+  return response.data;
+};
+
+export const fetchForecast = async (city) => {
+  const response = await axios.get(`${BASE_URL}/forecast`, {
+    params: {
+      q: city,
+      units: "metric",
+      appid: API_KEY,
+    },
+  });
+  return response.data;
+};
